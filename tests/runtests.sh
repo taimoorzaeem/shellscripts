6c650f03a13217740961ef2e1295108b1cef0bda
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2022-09-04 13:04:20 +0500

    Update runtests.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -8,23 +8,17 @@ out1=$( ../src/replspace test_dir )
 if [[ "$out1" == "Couldn't rename 'test_dir/foo bar.txt' to 'test_dir/foo_bar.txt' because 'test_dir/foo_bar.txt' already exists." ]]
 then
     passed=$(( passed + 1 ))
-else
-    echo "AAAAAAA"
 fi
 
 out2=$( ../src/replspace foo/ )
 if [[ "$out2" == "ERROR: Directory foo/ not found." ]]
 then
     passed=$(( passed + 1 ))
-else
-    echo "AAAAAAA"
 fi
 out3=$( ../src/replspace foo bar foobar )
 if [[ "$out3" == "Usage: replspace <directory-name>" ]]
 then
     passed=$((passed + 1 ))
-else
-    echo "AAAAAAA"
 fi
 
 echo "Test passed: $passed/$total "
